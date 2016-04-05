0a726d9aedfd0424d48e9739b1e456a01ee0813e
Tim Park <timfpark@gmail.com>
2016-04-05 11:05:32 -0700

    dump blob

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-module.exports = function (context, myBlob) {
-    context.log('Node.js blob trigger function processed blob: ' + myBlob.id);
+module.exports = function (context, locationBlob) {
+    context.log('Node.js blob trigger function processed blob:');
+    context.log(JSON.stringify(locationBlob));
     context.done();
 };
