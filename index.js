e5a9615f615c61ae31f59b13a05dd605a78421e1
Tim Park <timfpark@gmail.com>
2016-04-05 13:31:59 -0700

    bump

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function (context, rawLocationCSVBlob) {
-    context.log('Processing blob:');
+    context.log('Processing blob new:');
     context.log(JSON.stringify(context));
     context.log(JSON.stringify(locationBlob));
     context.done();
